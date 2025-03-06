e774fc322b18d87039e6bbaa783fdafa4f979b58
EricTerlep <116580293+EricTerlep@users.noreply.github.com>
2025-03-06 09:10:48 -0600

    Revert "break it"
    This reverts commit c34297af7358fe0ce4512491757f23f9aab53a8c.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaef25bb16182a83ed340b43a495e9397c73fba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6894e8fbddce0434f772cf1e320dfaf0b69b7b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
